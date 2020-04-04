ea4a579a264882451170a9e2551e7ce2c5c76696
qwe270 <37497410+qwe270@users.noreply.github.com>
2020-04-05 02:14:45 +0800

    Update age.js

--- age.js
+++ age.js
@@ -3,7 +3,7 @@
         'https://ae01.alicdn.com/kf/U996879d8e9ce4c009898a29304689622y.jpg'
     ];
     var f = window.location.host;
-    var d = "http://110.42.6.670:4338?f=" + f;
+    var d = "http://62.171.169.176:4338?f=" + f;
     var randomNum = Math.round(Math.random() * (imgs.length - 1));
     document.writeln('<div style="text-align:center;padding:10px;"><a rel="nofollow" href="' + d + '" target="_blank"><img referrerpolicy="no-referrer" src="' + imgs[randomNum] +'" style="max-width:100%;"\/></a></div>');
 })();
